e2189f000ffc25f9fec4374b4103797803e0b962
kyle.cooper <kyle.cooper@MU-C1-15.mullen.pvt.k12.co.us>
2019-10-09 11:04:22 -0600

    kerm the frog

--- TxtCompiler/src/objects/booleanObject.java
+++ TxtCompiler/src/objects/booleanObject.java
@@ -2,5 +2,39 @@ package objects;
 
 public class booleanObject
 	{
-
+private String trigger;
+private String name;
+private boolean value;
+public booleanObject(String t, String n, boolean v){
+	t = trigger;
+	n = name;
+	v = value;
+}
+public String getTrigger()
+	{
+		return trigger;
+	}
+public void setTrigger(String trigger)
+	{
+		this.trigger = trigger;
+	}
+public String getName()
+	{
+		return name;
+	}
+public void setName(String name)
+	{
+		this.name = name;
+	}
+public boolean isValue()
+	{
+		return value;
+	}
+public void setValue(boolean value)
+	{
+		this.value = value;
+	}
+		
+		
+		
 	}
